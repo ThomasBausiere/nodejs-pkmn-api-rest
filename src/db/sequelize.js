20394a8cd5e11f0eaf5a001f8492cb494c213187
Thomas <t.bausiere@gmail.com>
2024-08-16 15:18:40 +0200

    fixing db

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -4,7 +4,19 @@ const UserModel = require('../models/user')
 const pokemons = require('./mock-pokemon')
 const bcrypt = require('bcrypt')
   
-const sequelize = new Sequelize('pokedex', 'root', '', {
+let sequelize
+if(process.env.NODE_ENV ==='production'){
+  const sequelize = new Sequelize('lfcw08mfxg7kldqr', 'dw09cc7d0hj05a9v', 'n45qumvox7rjutt0', {
+    host: 'dcrhg4kh56j13bnu.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    dialect: 'mariadb',
+    dialectOptions: {
+      timezone: 'Etc/GMT-2',
+    },
+    logging: true
+  })
+}
+else{
+  const sequelize = new Sequelize('pokedex', 'root', '', {
   host: 'localhost',
   dialect: 'mariadb',
   dialectOptions: {
@@ -12,6 +24,9 @@ const sequelize = new Sequelize('pokedex', 'root', '', {
   },
   logging: false
 })
+}
+
+
   
 const Pokemon = PokemonModel(sequelize, DataTypes)
 const User = UserModel(sequelize, DataTypes) 
